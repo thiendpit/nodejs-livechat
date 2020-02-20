af398dbde4613a158ea9c14d18d28424f7e0b9da
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-02-20 10:20:04 +0700

    [1.0.0] Connect & create new item MongoDB.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,27 @@
+require("dotenv").config();
 import express from "express";
+import ConnectDB from "./models/config/connectDB";
+import ContactModel from './models/contact.model';
+
 let app = express();
+
+//connect to mongodb
+ConnectDB();
 
 let hostname = 'localhost';
 let port = 3000;
 
-app.use('/helloworld', (req, res) => {
-  res.send("<h1>Hello world !!</h1>");
+app.use('/test-database', async (req, res) => {
+  try{
+    let item = {
+      userId: "17071995",
+      contactId: "1707199517071995"
+    }
+    let contact = await ContactModel.createNew(item);
+    res.send(contact);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, hostname, () => {
