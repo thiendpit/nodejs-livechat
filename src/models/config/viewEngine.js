f81fe02d78f97271a7fa487685da691562585d3a
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-02-20 19:59:44 +0700

    [1.0.0] Add routes and controllers.

--- src/models/config/viewEngine.js
+++ src/models/config/viewEngine.js
@@ -2,6 +2,7 @@ import express from 'express';
 import expressEjsExtend from 'express-ejs-extend';
 
 // Config view engine for app
+// @param app from exactly express module
 
 let configViewEngine = (app) => {
   app.use(express.static("./src/public"));
