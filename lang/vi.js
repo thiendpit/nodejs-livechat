8733f8f34c68cce08eaf644294af0f5f42eee027
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-02-21 10:08:19 +0700

    [1.0.0] Register user part 1.

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation = {
+  email_incorrect: "Email phải có dạng example@gmail.com !",
+  gender_incorrect: "Trường giới tính bị sai",
+  password_incorrect: "Mật khẩu phải chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt.",
+  password_confirmation_incorrect: "Nhập lại mật khẩu không chính xác."
+};
